cdab03cc434c585aa914182b6cb66e652a775e26
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-10-23 15:55:06 +0200

    created method read in CourseDao Class

--- FMS_Course_Shop/src/fr/fms/TestDao.java
+++ FMS_Course_Shop/src/fr/fms/TestDao.java
@@ -6,11 +6,14 @@ import fr.fms.entities.Courses;
 public class TestDao {
 
 	public static void main(String[] args) {
-		
-		CourseDao courseDao = new CourseDao();
-		
 
-		
+		CourseDao courseDao = new CourseDao();
+
+		Courses course = courseDao.read(2);
+		course.setDuration(60);
+		System.out.println(courseDao.read(2));
+		courseDao.update(course);
+
 		System.out.println(courseDao.readAll());
 	}
 
--- FMS_Course_Shop/src/fr/fms/dao/CourseDao.java
+++ FMS_Course_Shop/src/fr/fms/dao/CourseDao.java
@@ -7,21 +7,22 @@ import java.util.ArrayList;
 
 import fr.fms.entities.Courses;
 
-public class CourseDao implements Dao<Courses>{
+public class CourseDao implements Dao<Courses> {
 
 	@Override
 	public boolean create(Courses obj) {
 		String strSQL = "INSERT INTO T_Courses ( name, description, duration, format, price, idCategory ) VALUES (?, ?, ?, ?, ?, ?);";
-		
-		try (PreparedStatement ps = connection.prepareStatement(strSQL)){
+
+		try (PreparedStatement ps = connection.prepareStatement(strSQL)) {
 			ps.setString(1, obj.getName());
 			ps.setString(2, obj.getDescription());
 			ps.setInt(3, obj.getDuration());
 			ps.setString(4, obj.getFormat());
 			ps.setDouble(5, obj.getPrice());
 			ps.setInt(6, obj.getIdCategory());
-			
-			if (ps.executeUpdate() == 1) return true;
+
+			if (ps.executeUpdate() == 1)
+				return true;
 		} catch (SQLException e) {
 			logger.severe("SQL problem when trying to create a new course : " + e.getMessage());
 		}
@@ -30,24 +31,43 @@ public class CourseDao implements Dao<Courses>{
 
 	@Override
 	public Courses read(int id) {
-		// TODO Auto-generated method stub
+		String strSQL = "SELECT * FROM T_Courses WHERE id=?;";
+
+		try (PreparedStatement ps = connection.prepareStatement(strSQL)) {
+			ps.setInt(1, id);
+
+			try (ResultSet rs = ps.executeQuery()) {
+
+				if (rs.next()) {
+					Courses course = new Courses(rs.getInt("id"), rs.getString("name"), rs.getString("description"),
+							rs.getInt("duration"), rs.getString("format"), rs.getDouble("price"),
+							rs.getInt("idCategory"));
+
+					return course;
+				}
+			}
+		} catch (SQLException e) {
+			logger.severe("SQL problem when trying to read a course : " + e.getMessage());
+		}
 		return null;
 	}
 
 	@Override
 	public boolean update(Courses obj) {
-		String strSQL = "UPDATE T_Courses SET name=?, description=?, duration=?, format=?, price=?, idCategory=?;";
-		
-		try(PreparedStatement ps = connection.prepareStatement(strSQL)){
+		String strSQL = "UPDATE T_Courses SET name=?, description=?, duration=?, format=?, price=?, idCategory=? WHERE id=?;";
+
+		try (PreparedStatement ps = connection.prepareStatement(strSQL)) {
 			ps.setString(1, obj.getName());
 			ps.setString(2, obj.getDescription());
 			ps.setInt(3, obj.getDuration());
 			ps.setString(4, obj.getFormat());
 			ps.setDouble(5, obj.getPrice());
 			ps.setInt(6, obj.getIdCategory());
-			
-			if (ps.executeUpdate() == 1) return true;
-		}catch(SQLException e) {
+			ps.setInt(7, obj.getId());
+
+			if (ps.executeUpdate() == 1)
+				return true;
+		} catch (SQLException e) {
 			logger.severe("SQL problem when trying to update a course : " + e.getMessage());
 		}
 		return false;
@@ -64,25 +84,19 @@ public class CourseDao implements Dao<Courses>{
 		ArrayList<Courses> courses = new ArrayList<>();
 		String strSQL = "SELECT * FROM T_Courses;";
 
-		try (PreparedStatement ps = connection.prepareStatement(strSQL)){
-			try(ResultSet rs = ps.executeQuery()){
-				while(rs.next()) {
-					Courses course = new Courses(
-							rs.getInt("id"), 
-							rs.getString("name"),
-							rs.getString("description"),
-							rs.getInt("duration"),
-							rs.getString("format"),
-							rs.getDouble("price"),
-							rs.getInt("idCategory")
-							);
+		try (PreparedStatement ps = connection.prepareStatement(strSQL)) {
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					Courses course = new Courses(rs.getInt("id"), rs.getString("name"), rs.getString("description"),
+							rs.getInt("duration"), rs.getString("format"), rs.getDouble("price"),
+							rs.getInt("idCategory"));
 					courses.add(course);
 				}
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			logger.severe("SQL problem when trying to read all courses : " + e.getMessage());
 		}
 		return courses;
 	}
-	
+
 }
