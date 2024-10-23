3c82df145c6a9f6bd73146587bf0e4ae363cbf29
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-10-23 12:15:17 +0200

    created Category Class

---
+++ FMS_Course_Shop/src/fr/fms/entities/Category.java
@@ -0,0 +1,31 @@
+package fr.fms.entities;
+
+public class Category {
+	private int id;
+	private String name;
+	
+	public Category(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Category : %d, %s", id, name);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
