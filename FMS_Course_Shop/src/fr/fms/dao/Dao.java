df945c1cedcbdc306ed29a1eed29baaa590fe45f
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-10-23 14:08:42 +0200

    created Dao Interface

---
+++ FMS_Course_Shop/src/fr/fms/dao/Dao.java
@@ -0,0 +1,21 @@
+package fr.fms.dao;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public interface Dao<T> {
+	public static Connection connection = DBConnection.getConnection();
+	public static final Logger logger = Logger.getLogger(Dao.class.getName());
+	
+	
+	public boolean create(T obj);
+	
+	public T read(int id);
+	
+	public boolean update(T obj);
+	
+	public boolean delete(T obj);
+	
+	public ArrayList<T> readAll();
+}
