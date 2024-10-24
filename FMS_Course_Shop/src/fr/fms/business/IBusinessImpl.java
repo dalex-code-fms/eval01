0a7da41a42f772619136aa4b068f478b0deb5686
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-10-24 12:29:20 +0200

    created IBusinessImpl Class and Shop Class

---
+++ FMS_Course_Shop/src/fr/fms/business/IBusinessImpl.java
@@ -0,0 +1,64 @@
+package fr.fms.business;
+
+import java.util.ArrayList;
+
+import fr.fms.entities.Category;
+import fr.fms.entities.Courses;
+
+public class IBusinessImpl implements IBusiness {
+
+	@Override
+	public void addToCart() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeFromCart() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<Courses> getCart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void displayCoursesByCat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayCoursesByformat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayCoursesByKeyword() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int createOrder(int idUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Courses readOneCourse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Category> readCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
