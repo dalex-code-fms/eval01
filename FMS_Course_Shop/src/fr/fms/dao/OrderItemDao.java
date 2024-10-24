acb27e27816c3693a985d95879d98f44b205087e
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-10-24 12:01:16 +0200

    created OrderItemDao Class

---
+++ FMS_Course_Shop/src/fr/fms/dao/OrderItemDao.java
@@ -0,0 +1,39 @@
+package fr.fms.dao;
+
+import java.util.ArrayList;
+
+import fr.fms.entities.OrderItem;
+
+public class OrderItemDao implements Dao<OrderItem> {
+
+	@Override
+	public boolean create(OrderItem obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public OrderItem read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(OrderItem obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(OrderItem obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<OrderItem> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
